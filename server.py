58cf42afa28d17aa660fa840250d0568bf361242
SempaiEcchi <minecraftm665@gmail.com>
2022-05-10 03:10:24 +0200

    local model

--- server.py
+++ server.py
@@ -2,11 +2,14 @@ import asyncio
 import os
 import time
 from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
-from keybert import KeyBERT
+from typing import List, Tuple
+
 import aiohttp_cors
 from aiohttp import web
 from aiohttp.helpers import get_running_loop
+from keybert import KeyBERT
 from sentence_transformers import SentenceTransformer
+from yake import yake
 
 routes = web.RouteTableDef()
 counter = 0
@@ -15,9 +18,12 @@ counter = 0
 @routes.post("/test")
 async def test(request):
     def task(text):
-        res = kw_model.extract_keywords(text, keyphrase_ngram_range=(1, 1), stop_words="english")
-        res = res + kw_model.extract_keywords(text, keyphrase_ngram_range=(2, 2), stop_words="english")
-        res = res + kw_model.extract_keywords(text, keyphrase_ngram_range=(3, 3), stop_words="english")
+
+        # kws: List[Tuple[str, float]] = kw_extractor.extract_keywords(text)
+        # seed = list(map(lambda x: str(x[0]), kws))
+
+        res = kw_model.extract_keywords(text, keyphrase_ngram_range=(2, 3), stop_words="english", top_n=7,)
+        # res = res + kw_model.extract_keywords(text, keyphrase_ngram_range=(3, 3), stop_words="english")
         return res
 
     # results: list[
@@ -33,6 +39,7 @@ async def test(request):
         tasks.append(get_running_loop().run_in_executor(thread_pool, lambda: task(text=sres["text"])))
     results = await asyncio.gather(*tasks)
     keyword_result = list(map(lambda x: dict(x), results))
+
     complete_scrape_results = []
     index = 0
     for scrap_res in scrape_results:
@@ -60,14 +67,19 @@ async def setup_app(app):
 app = None
 thread_pool = None
 kw_model = None
+kw_extractor = None
 
 
 async def run():
     global app
+    global kw_extractor
     global thread_pool
     global kw_model
-    sentence_model = SentenceTransformer(os.path.abspath("")+"/storage/all-MiniLM-L6-v2/")
+    sentence_model = SentenceTransformer(os.path.abspath("") + "/storage/all-MiniLM-L12-v2/")
     kw_model = KeyBERT(model=sentence_model)
+    kw_extractor = yake.KeywordExtractor(dedupLim=0.9,
+                                         dedupFunc='seqm', top=10)
+
     thread_pool = ThreadPoolExecutor(max_workers=50)
     app = web.Application()
     app.on_startup.append(setup_app)
